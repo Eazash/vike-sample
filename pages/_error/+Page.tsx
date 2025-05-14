fbe8863e2a59b7ad807a3187e9dc30563b722100
Eazash <eazash22@gmail.com>
2025-05-14 16:45:23 +0300

    chore: try to replicate error

--- pages/_error/+Page.tsx
+++ pages/_error/+Page.tsx
@@ -1,19 +1,38 @@
 import { usePageContext } from "vike-react/usePageContext";
 
-export default function Page() {
-  const { is404 } = usePageContext();
-  if (is404) {
+export { Page }
+/* Or:
+import { usePageContext } from 'vike-vue/usePageContext'
+import { usePageContext } from 'vike-solid/usePageContext'
+*/
+
+function Page() {
+  const pageContext = usePageContext()
+
+  let msg: string // Message shown to the user
+  const { abortReason, abortStatusCode } = pageContext
+  if (typeof abortReason === 'string') {
+    // Handle `throw render(abortStatusCode, `You cannot access ${someCustomMessage}`)`
+    msg = abortReason
+  }
+  else if (abortStatusCode === 403) {
+    // Handle `throw render(403)`
+    msg = 'You cannot access this page because you don\'t have enough privileges.'
+  }
+  else if (abortStatusCode === 401) {
+    // Handle `throw render(401)`
+    msg = 'You cannot access this page because you aren\'t logged in. Please log in.'
+  }
+  else {
+    // Fallback error message
+    msg = pageContext.is404
+      ? 'This page doesn\'t exist.'
+      : 'Something went wrong. Try again (later).'
+  }
     return (
       <>
-        <h1>404 Page Not Found</h1>
+        <h1>{msg}</h1>
         <p>This page could not be found.</p>
       </>
     );
-  }
-  return (
-    <>
-      <h1>500 Internal Server Error</h1>
-      <p>Something went wrong.</p>
-    </>
-  );
 }
---
+++ renderer/+config.ts
@@ -0,0 +1,6 @@
+import type { Config } from "vike/types"
+import * as config from "@vite-plugin-vercel/vike/config"
+
+export default {
+  extends: config,
+} satisfies Config
